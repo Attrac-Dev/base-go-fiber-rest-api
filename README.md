d498431818628ffe0e609f50edb90271ddcc4b10
Skyler Saville <skylersaville@gmail.com>
2022-02-12 23:46:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+testing out the Go Fiber framework, which is very similar to Express.js
+
+every import needs to be added using the <br>
+`go get -u <some github url>`
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        err := c.SendString("API is up!")
+		return err
+    })
+
+	// listen on port:3000
+    app.Listen(":3000")
+}
