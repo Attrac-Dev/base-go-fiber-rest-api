c6d11aaca005821edde7dbfae2876612b4ada17a
Skyler Saville <skylersaville@gmail.com>
2022-03-26 03:47:04 -0600

    added the router setup to the main.go file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/skyler-saville/base-api-fiber/database"
+    "github.com/skyler-saville/base-api-fiber/router"
 )
 
 
@@ -12,6 +13,10 @@ func main() {
     // attempt database connection
     database.ConnectDB()
 
+    // setup router
+    router.SetupRoutes(app)
+
+    // test route that has limited functionality
     app.Get("/", func(c *fiber.Ctx) error {
         err := c.SendString("API is up!!!")
 		return err
